4e12ad228c71b115239bbd1c835c2df618a30464
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-02-20 13:38:22 +0700

    add featureB Branch

--- 2_Version Control and Branch Management/praktikum/sesi1.js
+++ 2_Version Control and Branch Management/praktikum/sesi1.js
@@ -3,5 +3,6 @@ console.log("Add Push Method");
 console.log("Fixing Stash Method");
 console.log("Apply Stash Method 1");
 console.log("Apply Stash Method 2");
+console.log("Add FeatureB Branch");
 
 
