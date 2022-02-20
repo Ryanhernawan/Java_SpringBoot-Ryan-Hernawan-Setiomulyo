363c505ab8f85b940a8b4e0d06f1650aa706d3bc
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-02-20 17:33:24 +0700

    add featureA Sub

--- 2_Version Control and Branch Management/praktikum/sesi1.js
+++ 2_Version Control and Branch Management/praktikum/sesi1.js
@@ -4,6 +4,6 @@ console.log("Fixing Stash Method");
 console.log("Apply Stash Method 1");
 console.log("Apply Stash Method 2");
 console.log("Add FeatureA Branch");
-
+console.log("Add FeatureA Sub");
 
 
