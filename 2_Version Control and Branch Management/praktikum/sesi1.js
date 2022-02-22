969ec30f44bc41e633595d2bcc49be26ae9ec876
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-02-22 10:58:40 +0700

    update branch featureB

--- 2_Version Control and Branch Management/praktikum/sesi1.js
+++ 2_Version Control and Branch Management/praktikum/sesi1.js
@@ -4,5 +4,6 @@ console.log("Fixing Stash Method");
 console.log("Apply Stash Method 1");
 console.log("Apply Stash Method 2");
 console.log("Add FeatureB Branch");
+console.log("Merge FeatureB ke Master");
 
 
