70174e5b05f03ee1b9ad6966628529781b7d7afe
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-02-23 16:12:52 +0700

    add new folder

---
+++ 4_Basic Programing/praktikum/src/main/java/com/alta/Task/Task1.java
@@ -0,0 +1,7 @@
+package com.alta.Task;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 4_Basic Programing/praktikum/src/main/java/com/alta/Task/main.java
@@ -0,0 +1,9 @@
+package com.alta.Task;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
