34dd5af7d8de7bd3d81642b1079108e318ec783d
Ryanhernawan <54749133+Ryanhernawan@users.noreply.github.com>
2022-02-23 16:13:50 +0700

    Delete 4_Basic Programing/praktikum directory

--- 4_Basic Programing/praktikum/src/main/java/com/alta/Task/Task1.java
+++
@@ -1,7 +0,0 @@
-package com.alta.Task;
-
-public class Task1 {
-    public static void main(String[] args) {
-
-    }
-}
--- 4_Basic Programing/praktikum/src/main/java/com/alta/Task/main.java
+++
@@ -1,9 +0,0 @@
-package com.alta.Task;
-
-import java.util.Scanner;
-
-public class main {
-    public static void main(String[] args) {
-
-    }
-}
