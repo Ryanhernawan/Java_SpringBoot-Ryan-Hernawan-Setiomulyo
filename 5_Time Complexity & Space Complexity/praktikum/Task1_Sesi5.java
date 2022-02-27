cd22b990f00485e52943539de0a849fc164d2bf1
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-02-27 13:05:10 +0700

    update tugas

--- 5_Time Complexity & Space Complexity/praktikum/Task1_Sesi5.java
+++ 5_Time Complexity & Space Complexity/praktikum/Task1_Sesi5.java
@@ -1,26 +1,28 @@
+//package com.alta.Sesi_5;
+
 import java.util.Scanner;
 
 public class Task1_Sesi5 {
-    public  static boolean PrimeNumber(int bilangan){
-       for (int i= 2; i<=bilangan/2; i++){
-           if (bilangan % i == 0){
-               break;
-           }
-            return true;
+    public static  boolean PrimeNumber(int count){
+        boolean result = true;
+        if (count < 2){
+            return false;
         }
-
-        return false;
+        for (int i=2; i<count; i++){
+            if (count % i == 0){
+                result = false;
+            }
+        }
+        return result;
     }
-
 
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int bilangan;
-
+        int number;
         System.out.println("Masukan Bilangan : ");
-        bilangan  = input.nextInt();
-        if (PrimeNumber(bilangan)){
+        number = input.nextInt();
+        if (PrimeNumber(number)){
             System.out.println("Bilangan Prima");
         }
         else {
