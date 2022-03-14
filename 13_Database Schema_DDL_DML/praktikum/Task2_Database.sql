fc071fd4e93c649eddda4e3b40c970dacaa5e3ac
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-03-14 20:25:43 +0700

    update task

--- 13_Database Schema_DDL_DML/praktikum/Task2_Database.sql
+++ 13_Database Schema_DDL_DML/praktikum/Task2_Database.sql
@@ -4,78 +4,78 @@ USE alta_online_shop;
 
 -- CREATING TABLE
 
-create table users(
-ID int not null auto_increment primary key,
-Name varchar(255),
-status smallint,
-dob date,
-gender char(1),
-created_at timestamp,
-update_at timestamp
-);
-
-create table product(
-ID int auto_increment primary key,
-product_type_id int,
-operator_id int,
-code varchar(50),
-nama varchar(100),
-status smallint,
-create_at timestamp,
-update_at TIMESTAMP
-);
-
-create table product_types(
-ID int not null auto_increment primary key,
+CREATE TABLE users(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(255),
-create_at timestamp,
+status SMALLINT,
+dob DATE,
+gender CHAR(1),
+created_at TIMESTAMP,
 update_at TIMESTAMP
 );
 
-create table product_description(
-ID int not null auto_increment primary key,
+CREATE TABLE product(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+product_type_id INT,
+operator_id INT,
+code VARCHAR(50),
+nama VARCHAR(100),
+status SMALLINT,
+created_at TIMESTAMP,
+updated_at TIMESTAMP
+);
+
+CREATE TABLE product_types(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255),
+created_at TIMESTAMP,
+updated_at TIMESTAMP
+);
+
+CREATE TABLE product_description(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 description TEXT,
-create_at timestamp,
-update_at TIMESTAMP
+created_at TIMESTAMP,
+updated_at TIMESTAMP
 );
 
-create table operator(
-ID int  not null auto_increment primary key,
+CREATE TABLE operator(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(255),
-create_at TIMESTAMP,
-update_at TIMESTAMP
+created_at TIMESTAMP,
+updated_at TIMESTAMP
 );
 
 CREATE TABLE transaction(
-ID int not null auto_increment primary key,
-User_Id int,
-Payment_Method_id int,
-status varchar(10),
-total_qty int(11),
-total_price numeric(25,2),
-created_at timestamp,
-uptdae_at timestamp
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_Id INT,
+payment_Method_id INT,
+status VARCHAR(10),
+total_qty INT(11),
+total_price NUMERIC(25,2),
+created_at TIMESTAMP,
+updated_at TIMESTAMP
 );
 
 CREATE TABLE transaction_detail(
-ID int not null auto_increment primary key,
-status varchar(10),
-qty int(11),
-price numeric(25,2),
-created_at timestamp,
-uptdae_at timestamp
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+status VARCHAR(10),
+qty INT(11),
+price NUMERIC(25,2),
+created_at TIMESTAMP,
+uptdated_at TIMESTAMP
 );
 
 CREATE TABLE payment_methods(
-ID int not null auto_increment primary key,
-name varchar(255),
-status smallint,
-created_at timestamp,
-update_at timestamp
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255),
+status SMALLINT,
+created_at TIMESTAMP,
+updated_at TIMESTAMP
 );
 
 CREATE TABLE kurir(
-ID int not null auto_increment primary key,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(255),
 created_at TIMESTAMP,
 updated_at TIMESTAMP
