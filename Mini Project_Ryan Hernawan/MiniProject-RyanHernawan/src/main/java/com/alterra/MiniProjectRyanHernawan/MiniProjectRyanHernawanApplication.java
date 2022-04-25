6d111a69ffca941a8b4e4eef605fcc46d4ab9729
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-04-25 12:23:42 +0700

    update mini project

---
+++ Mini Project_Ryan Hernawan/MiniProject-RyanHernawan/src/main/java/com/alterra/MiniProjectRyanHernawan/MiniProjectRyanHernawanApplication.java
@@ -0,0 +1,13 @@
+package com.alterra.MiniProjectRyanHernawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectRyanHernawanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectRyanHernawanApplication.class, args);
+	}
+
+}
